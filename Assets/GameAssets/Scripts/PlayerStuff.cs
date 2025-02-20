e4181ec7e773a3ac9d2643812e8fceb0738a2e69
BigDaddyStinger <mattwalsh5697@gmail.com>
2025-02-20 09:48:32 -0600

    Movement Done but Keeps Rolling

--- Assets/GameAssets/Scripts/PlayerStuff.cs
+++ Assets/GameAssets/Scripts/PlayerStuff.cs
@@ -7,6 +7,8 @@ public class PlayerStuff : MonoBehaviour
     [SerializeField] float jumpForce = 1.0f;
     Rigidbody rb;
     Vector3 movement;
+    [SerializeField] bool grounded;
+    [SerializeField] Transform myCamera;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,13 +23,36 @@ public class PlayerStuff : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
 
+        Vector3 camForward = myCamera.forward;
+        Vector3 camRight = myCamera.right;
+
+        camForward.y = 0;
+        camForward.Normalize();
+
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 forwardRelativeMovementVector = yInput * camForward;
+        Vector3 rightRelativeMovementVector = xInput * camRight;
+
+
         movement = new Vector3(xInput, 0, yInput) * speed * Time.deltaTime;
         movement.y = rb.linearVelocity.y;
 
-        if(Input.GetButtonDown("Jump"))
+        grounded = Physics.Raycast(transform.position, Vector3.down, 1);
+
+
+        if(Input.GetButtonDown("Jump") && grounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+
+
+        if(xInput != 0 && yInput != 0)
+        {
+        transform.forward = new Vector3(xInput, 0, yInput);
+        }
+
     }
 
     private void FixedUpdate()
